17dc116475425e0ff6296d0a7122894954ff46b3
aeryen <aeryen@gmail.com>
2017-03-22 03:52:49 -0500

    minor tweak for doc level

--- aa.py
+++ aa.py
@@ -8,7 +8,7 @@ from evaluators import eval_ml_origin as evaler_one
 
 if __name__ == "__main__":
 
-    input_component = "OneChannel"
+    input_component = "OneChannel_DocLevel"
 
     if input_component == "OneChannel":
         dater = dh.DataHelper(doc_level="sent", train_holdout=0.80, embed_type="glove", embed_dim=300)
@@ -16,6 +16,10 @@ if __name__ == "__main__":
     elif input_component == "SixChannel":
         dater = dh6.DataHelperMulMol6(doc_level="sent", train_holdout=0.80)
         ev = evaler.evaler()
+    elif input_component == "OneChannel_DocLevel":
+        dater = dh.DataHelper(doc_level="sent", train_holdout=0.80, embed_type="glove", embed_dim=300,
+                              target_doc_len=128, target_sent_len=128)
+        ev = evaler_one.evaler()
     else:
         raise NotImplementedError
 
@@ -34,13 +38,13 @@ if __name__ == "__main__":
     # *ParallelJoinedConv
     # *NCrossSizeParallelConvNFC
     ################################################
-    tt = tr.TrainTask(data_helper=dater, input_component=input_component, exp_name="NCrossSizeParallelConvNFC",
-                      filter_sizes=[[3, 4, 5], [3, 4, 5]], batch_size=64, dataset="ML", evaluate_every=5000, checkpoint_every=5000)
+    tt = tr.TrainTask(data_helper=dater, input_component=input_component, exp_name="DocLevelBasic",
+                      batch_size=32, dataset="ML", evaluate_every=5000, checkpoint_every=5000)
     start = timer()
     # n_fc variable controls how many fc layers you got at the end, n_conv does that for conv layers
 
-    ts = tt.training(num_filters=100, dropout_keep_prob=0.7, n_steps=100000, l2_lambda=0.1, dropout=True,
-                batch_normalize=False, elu=False, n_conv=2, fc=[])
+    ts = tt.training(filter_sizes=[[3, 4, 5]], num_filters=100, dropout_keep_prob=0.7, n_steps=100000, l2_lambda=0.1, dropout=True,
+                batch_normalize=False, elu=False, n_conv=1, fc=[])
     end = timer()
     print(end - start)
 
